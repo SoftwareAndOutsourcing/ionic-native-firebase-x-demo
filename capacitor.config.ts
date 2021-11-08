30405eb6e17e30365a8ac8ca4b9c0783fa99baa7
SoftwareAndOutsourcing <5988222+SoftwareAndOutsourcing@users.noreply.github.com>
2021-11-08 21:34:59 +0100

    Add Ionic starter

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-native-firebase-x-demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
